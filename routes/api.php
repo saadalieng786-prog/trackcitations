082dee40428a14accc8e7ea073a240950e9d2eb1
Saad ali <saadalieng786@gmail.com>
2026-08-03 14:51:52 -0700

    move api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController as APIUserController;
+use App\Http\Controllers\Api\DriverController as APIDriverController;
+use App\Http\Controllers\Api\CompanyController as APICompanyController;
+use App\Http\Controllers\Api\AttorneyController as APIAttorneyController;
+use App\Http\Controllers\Api\TicketAttachmentController as APITicketAttachmentController;
+use App\Http\Controllers\Api\TicketNoteController as APITicketNoteController;
 
 Route::middleware('auth:sanctum')->group(function () {
 
+});
+
+
+Route::middleware('auth')->name('api.')->group(function () {
+    Route::get('users/exclude/{conversation?}', [APIUserController::class, 'exclude'])->name('users.exclude');
+    Route::get('drivers', [APIDriverController::class, 'index'])->name('driver.index');
+    Route::get('companies', [APICompanyController::class, 'index'])->name('company.index');
+    Route::get('attorneys', [APIAttorneyController::class, 'index'])->name('attorney.index')->middleware('auth');
+    Route::post('ticket/{ticket}/attach', [APITicketAttachmentController::class, 'store'])->name('tickets-attach.store')->middleware('auth');
+    Route::post('ticket/{ticket}/note', [APITicketNoteController::class, 'store'])->name('tickets-note.store')->middleware('auth');
+
+    Route::get('/conversations', [ConversationController::class, 'index']);
+    Route::post('/conversations', [ConversationController::class, 'store'])->name('conversations.store');
+    Route::post('/conversations/{conversation}/messages', [MessageController::class, 'store']);
+    Route::post('/messages/{message}/read', [ReadController::class, 'store']);
+    Route::get('/messages/{message}/reads', [ReadController::class, 'getMessageReads']);
+
+    Route::post('setSessionCompanies', [HomeController::class, 'setSessionCompanies'])->name('setSessionCompanies');
 });
--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,7 @@ use App\Integrations\Salesforce\SalesforceClient;
 use App\Models\User;
 use App\Models\SalesForce;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController as APIUserController;
-use App\Http\Controllers\Api\DriverController as APIDriverController;
-use App\Http\Controllers\Api\CompanyController as APICompanyController;
-use App\Http\Controllers\Api\AttorneyController as APIAttorneyController;
-use App\Http\Controllers\Api\TicketAttachmentController as APITicketAttachmentController;
-use App\Http\Controllers\Api\TicketNoteController as APITicketNoteController;
 use App\Http\Controllers\CourtDateController;
-
 use App\Http\Controllers\ConversationController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ReadController;
@@ -169,24 +162,6 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('dashboard', [DriverController::class, 'dashboard'])->name('dashboard');
         Route::resource('tickets', DriverTicketController::class);
     });
-});
-
-// API Routes. TODO: Move to api.php
-Route::group(['middleware' => 'auth', 'prefix' => 'api', 'as' => 'api.'], function () {
-    Route::get('users/exclude/{conversation?}', [APIUserController::class, 'exclude'])->name('users.exclude');
-    Route::get('drivers', [APIDriverController::class, 'index'])->name('driver.index');
-    Route::get('companies', [APICompanyController::class, 'index'])->name('company.index');
-    Route::get('attorneys', [APIAttorneyController::class, 'index'])->name('attorney.index')->middleware('auth');
-    Route::post('ticket/{ticket}/attach', [APITicketAttachmentController::class, 'store'])->name('tickets-attach.store')->middleware('auth');
-    Route::post('ticket/{ticket}/note', [APITicketNoteController::class, 'store'])->name('tickets-note.store')->middleware('auth');
-
-    Route::get('/conversations', [ConversationController::class, 'index']);
-    Route::post('/conversations', [ConversationController::class, 'store'])->name('conversations.store');
-    Route::post('/conversations/{conversation}/messages', [MessageController::class, 'store']);
-    Route::post('/messages/{message}/read', [ReadController::class, 'store']);
-    Route::get('/messages/{message}/reads', [ReadController::class, 'getMessageReads']);
-
-    Route::post('setSessionCompanies', [HomeController::class, 'setSessionCompanies'])->name('setSessionCompanies');
 });
 
 
